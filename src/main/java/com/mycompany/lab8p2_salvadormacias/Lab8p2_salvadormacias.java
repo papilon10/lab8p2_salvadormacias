c0af8ffbcda1cdfe443ad1493de87c4e1755d77c
papilon10 <140213599+papilon10@users.noreply.github.com>
2023-09-08 13:34:50 -0600

    creacion proyecto

---
+++ src/main/java/com/mycompany/lab8p2_salvadormacias/Lab8p2_salvadormacias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab8p2_salvadormacias;
+
+/**
+ *
+ * @author papilon10
+ */
+public class Lab8p2_salvadormacias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
